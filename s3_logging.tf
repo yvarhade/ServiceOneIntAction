f7c762884fb0d860c89bfad0d2f0bc02bfa8fc0f
iactest <103730049+iactest@users.noreply.github.com>
2022-06-14 10:24:16 +0530

    Update s3_logging.tf

--- s3_logging.tf
+++ s3_logging.tf
@@ -2,47 +2,7 @@ resource "aws_s3_bucket" "examplebucket" {
   bucket = "examplebuckettftest"
 }
 
-resource "aws_s3_bucket" "examplebucket23" {
-  bucket = "examplebuckettftest23"
-}
 
-resource "aws_s3_bucket" "examplebucket20" {
-  bucket = "examplebuckettftest20"
-}
-
-resource "aws_s3_bucket" "examplebucket30" {
-  bucket = "examplebuckettftest30"
-}
-
-resource "aws_s3_bucket" "examplebucket50" {
-  bucket = "examplebuckettftest50"
-}
-
-resource "aws_s3_bucket" "examplebucket60" {
-  bucket = "examplebuckettftest60"
-}
-
-resource "aws_s3_bucket" "examplebucket61" {
-  bucket = "examplebuckettftest61"
-}
-
-resource "aws_s3_bucket" "examplebucket70" {
-  bucket = "examplebuckettftest70"
-}
-
-resource "aws_s3_bucket" "examplebucket71" {
-  bucket = "examplebuckettftest71"
-}
-
-resource "aws_s3_bucket" "examplebucket73" {
-  bucket = "examplebuckettftest73"
-}
-
-resource "aws_s3_bucket" "examplebucket74" {
-  bucket = "examplebuckettftest74"
-}
-
-
-resource "aws_s3_bucket" "examplebucket80" {
-  bucket = "examplebuckettftest80"
+resource "aws_s3_bucket" "examplebucket100" {
+  bucket = "examplebuckettftest100"
 }
